5943ae1382852545f8ee48e9947d3561fab0a180
Nael Chelouah <naelchelouah@gmail.com>
2026-07-20 21:58:54 +0200

    Fix service worker: scope caching to same-origin GET, cache navigation responses

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'daily-v1'
-const STATIC_ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png']
+const STATIC_ASSETS = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)))
@@ -18,8 +18,20 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   const { request } = event
 
+  if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) {
+    return
+  }
+
   if (request.mode === 'navigate') {
-    event.respondWith(fetch(request).catch(() => caches.match(request).then((res) => res || caches.match('/'))))
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const clone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, clone))
+          return response
+        })
+        .catch(() => caches.match(request).then((res) => res || caches.match('/')))
+    )
     return
   }
 
